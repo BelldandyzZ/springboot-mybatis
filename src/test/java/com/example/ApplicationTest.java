11594901bfc3d2a07a636afa2fd3f06ae09295f0
Yuno <1014893924@qq.com>
2023-07-16 14:10:48 +0800

    环境搭建

---
+++ src/test/java/com/example/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
